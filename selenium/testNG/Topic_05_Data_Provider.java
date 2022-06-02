d87acfb1ac2926c2739a3ac7705e7d69cfe121dd
133864 <cynhi@fossil.com>
2022-06-03 00:02:19 +0700

    Add Topic 05

--- selenium/testNG/Topic_05_Data_Provider.java
+++ selenium/testNG/Topic_05_Data_Provider.java
@@ -24,54 +24,49 @@ public class Topic_05_Data_Provider {
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
 		driver = new FirefoxDriver();
-		
+
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test(dataProvider = "data")
 	public void register(String userName, String password) {
-		 System.out.println(userName);
-		 System.out.println(password);
-	 }
-	
+		System.out.println(userName);
+		System.out.println(password);
+	}
+
 	@Test(dataProvider = "data")
-    public void login(String userName, String password) {
+	public void login(String userName, String password) {
 		driver.get("http://live.techpanda.org/index.php/customer/account/login");
 		driver.findElement(emailTextbox).sendKeys(userName);
 		driver.findElement(pwdTextbox).sendKeys(password);
 		driver.findElement(loginButton).click();
-		
+
 		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='col-1']//p")).getText().contains(userName));
-		
+
 		driver.findElement(By.xpath("//header[@id='header']//span[text()='Account']")).click();
 		driver.findElement(By.xpath("//a[text()='Log Out']")).click();
-  }
+	}
 
-  @DataProvider(name="data")
-  public Object[][] loginData(Method method) {
-	  Object[][] obj = null;
-      if(method.getName().equals("register")) {
-    	  obj = new Object[][]{ 
-    		  		{ "selenium_12@gmail.com", "111111" },
-    		    	{ "selenium_13@gmail.com", "111111" },
-    		    	{ "selenium_14@gmail.com", "111111" }
-    		      
-    		    };
-      } else {
-    	  obj = new Object[][]{ 
-		  		{ "selenium_11_01@gmail.com", "111111" },
-		    	{ "selenium_11_02@gmail.com", "111111" },
-		    	{ "selenium_11_03@gmail.com", "111111" }
-    	  };
-      }
-	  
-    return obj;
-    	 
-  }
-  
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
+	@DataProvider(name = "data")
+	public Object[][] loginData(Method method) {
+		Object[][] obj = null;
+		if (method.getName().equals("register")) {
+			obj = new Object[][] { { "selenium_12@gmail.com", "111111" }, { "selenium_13@gmail.com", "111111" },
+					{ "selenium_14@gmail.com", "111111" }
+
+			};
+		} else {
+			obj = new Object[][] { { "selenium_11_01@gmail.com", "111111" }, { "selenium_11_02@gmail.com", "111111" },
+					{ "selenium_11_03@gmail.com", "111111" } };
+		}
+
+		return obj;
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
 }
